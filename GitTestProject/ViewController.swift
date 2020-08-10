b0f1863ee7cabfa6a88fb136e5b757f1c4a7ebfa
Burak Baran <baranburak17@gmail.com>
2020-08-11 00:20:24 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Burak on 11.08.2020.
+//  Copyright © 2020 Burak Baran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
